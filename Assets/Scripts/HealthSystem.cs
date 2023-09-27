a136480a32ba7b263f207cca4c7ec10d881ad5e4
MTL\ldunphy <liam.dunphy@mymail.champlain.edu>
2023-09-27 11:12:13 -0400

    Initial commit, health, player movement

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int Health = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Health--;
+            Debug.Log(Health);
+        }
+
+        if (Health <= 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float moveAcceleration = 10;
+
+    public float jumpSpeed = 5;
+    public float jumpMaxTime = 0.5f;
+    private float jumpTimer = 0;
+
+    public Transform attackRangeAnchor;
+    public Vector3 attackRangeSize;
+
+    private CharacterController characterController;
+
+    private Vector2 moveInput = Vector2.zero;
+    private Vector2 currentHorizontalVelocity = Vector2.zero;
+    private float currentVerticalVelocity = 0;
+
+    private bool jumpInputPressed = false;
+
+    private bool isJumping = false;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        currentHorizontalVelocity = Vector2.Lerp(currentHorizontalVelocity, moveInput * moveSpeed, Time.deltaTime * moveAcceleration);
+
+        if(isJumping == false)
+        {
+            currentVerticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+            if(characterController.isGrounded && currentVerticalVelocity < 0 )
+            {
+                currentVerticalVelocity = Physics.gravity.y * Time.deltaTime;
+            }
+        }
+        else
+        {
+            jumpTimer += Time.deltaTime;
+
+            if ( jumpTimer >= jumpMaxTime)
+            {
+                isJumping = false;
+            }
+        }
+
+        Vector3 currentVelocity = new Vector3(currentHorizontalVelocity.x, currentVerticalVelocity, currentHorizontalVelocity.y);
+        characterController.Move(currentVelocity * Time.deltaTime);
+
+        RotateCharacter();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump (InputValue value)
+    {
+        jumpInputPressed = value.Get<float>() > 0;
+
+        if (jumpInputPressed)
+        {
+            if(characterController.isGrounded)
+            {
+                isJumping = true;
+
+                jumpTimer = 0;
+
+                currentVerticalVelocity = jumpSpeed;
+            }
+        }
+        else
+        {
+            if (isJumping)
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+    public void OnAttack(InputValue value)
+    {
+       OverlapAttackCheck();
+    }
+
+    private void OverlapAttackCheck()
+    {
+        Collider[] overlapItems = Physics.OverlapBox(
+                                        attackRangeAnchor.transform.position,
+                                        attackRangeSize / 2,
+                                        attackRangeAnchor.transform.rotation,
+                                        Physics.AllLayers,
+                                        QueryTriggerInteraction.Ignore);
+
+        if (overlapItems.Length > 0)
+        {
+            foreach (Collider item in overlapItems)
+            {
+                Vector3 direction = item.transform.position - transform.position;
+                item.SendMessage("OnPlayerAttack", direction, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackRangeAnchor != null)
+        {
+            Gizmos.color = new Vector4(1, 0, 0, 0.5f);
+            Gizmos.DrawCube(attackRangeAnchor.transform.position, attackRangeSize);
+        }
+    }
+
+    private void RotateCharacter()
+    {
+        Vector3 lookDirection = new Vector3(currentHorizontalVelocity.x, 0, currentHorizontalVelocity.y).normalized;
+
+        if (lookDirection.magnitude > 0.001f)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(lookDirection);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * moveAcceleration);
+        }
+    }
+}
