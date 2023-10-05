ad12443274cab24f1689c4d242a2ea96f9456bd0
MTL\ldunphy <liam.dunphy@mymail.champlain.edu>
2023-10-05 16:57:32 -0400

    AI pathing added

---
+++ Assets/Scripts/MoveToDestination.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveToDestination : MonoBehaviour
+{
+    public Transform Destination;
+    NavMeshAgent iceMonster;
+
+    private void Awake()
+    {
+        iceMonster = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        
+
+        iceMonster.destination = Destination.position;
+    }
+}
